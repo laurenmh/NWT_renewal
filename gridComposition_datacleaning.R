30e136aff1634f43ec971e63be01881ceb13b644
Lauren Hallett <lauren.hallett@berkeley.edu>
2015-11-05 16:21:34 -0700

    species cleaning and added preliminary synchrony

--- gridComposition_datacleaning.R
+++ gridComposition_datacleaning.R
@@ -11,7 +11,12 @@ datpath = "~/Dropbox/NWT_data/" # this likely will be different for different fo
 #read in the data
 rawdat <- read.csv(file.path(datpath, 'saddle_grid_point_quadrat_89-13.csv')) %>%
   tbl_df() %>%
-  select(year, plot, point, hit_type, USDA_code)
+  select(year, plot, point, hit_type, USDA_code) %>%
+  #Hope's species conversion key doesn't include "CAREX1-CAREX3" so renaming them all CAREX
+  mutate(USDA_code=as.character(USDA_code),
+         USDA_code = ifelse(USDA_code=="CAREX1", "CAREX", USDA_code),
+         USDA_code = ifelse(USDA_code=="CAREX2", "CAREX", USDA_code),
+         USDA_code = ifelse(USDA_code=="CAREX3", "CAREX", USDA_code))
 
 #create a unique species key
 sppkey<-read.csv(file.path(datpath, "pspecies.mw.data.csv")) %>%
@@ -30,10 +35,15 @@ vegtype.key <- read.csv(file.path(datpath, 'NWT_Saddle_ComType.csv')) %>%
 #will need to fill those in as 0s in future, or find another way to think about them
 saddat<- merge(rawdat, sppkey, all.x=T) %>%
   filter(hit_type!="middle1", hit_type!="middle2") %>%
- filter(hit_type=="bottom") %>%
-  select(year, plot, point, USDA_code, USDA_name, hit_type, is_veg) %>%
+  filter(hit_type=="bottom") %>%
+  select(year, plot, point, USDA_code, USDA_name, hit_type, is_veg, category, Weber_family) %>%
   mutate(plot_point_hit = paste(plot, point, hit_type, sep="_")) %>%
-  mutate(abund=1)
+  mutate(abund=1) %>%
+  tbl_df() %>%
+  mutate(func="Forb", func=ifelse())
+
+unique(saddat$Weber_family)
+
 
 #calculate "turnover" - will only give 2 values - 1 if the species changed, 0 if it did not
 #for right now, included bare as a "species" - should do a more refined analysis of transitions from bare to veg, and veg to bare
@@ -52,10 +62,91 @@ sadturn2<-merge(sadturn2, vegtype.key, by="plot")
 sadturn3<-merge(sadturn, saddat, all.x=T) %>%
   group_by(year, plot, is_veg) %>%
   summarize(percent_change=sum(total)) %>%
-  filter(year==1996 | year == 1997 | year==1990 | year == 2011 | year ==2012 | year ==2013)
+  filter(year==1996 | year == 1997 | year==1990 | year == 2011 | year ==2012 | year ==2013) %>%
+  tbl_df()
 sadturn3<-merge(sadturn3, vegtype.key, by="plot")
 
 
 ggplot(sadturn2, aes(x=year, y=percent_change, group=plot)) + geom_point() + facet_wrap(~class_3)
 ggplot(sadturn3, aes(x=year, y=percent_change, group=plot)) + geom_point() + facet_wrap(class_3~is_veg)
+
+##some different synchrony metrics
+
+#synchrony at the species level
+datspp<- merge(saddat, vegtype.key, by="plot") %>%
+  filter(is_veg > 0, category != "nonveg") %>%
+  group_by(year, plot, USDA_code, orig_cluster, class_3, category, Weber_family) %>%
+    summarize(abund=sum(abund)) %>%
+  tbl_df() 
+
+sppsynch <- synchrony(datspp, time.var="year", species.var = "USDA_code", abundance.var="abund", replicate.var="plot")
+sppsynch2 <- merge(sppsynch, vegtype.key, by="plot") %>%
+  mutate(type="species")
+ggplot(sppsynch2, aes(x=synchrony)) + geom_bar() + facet_wrap(~orig_cluster)
+ggplot(sppsynch2, aes(x=synchrony)) + geom_bar() + facet_wrap(~class_3)
+
+#synchrony at the category level
+datcat<- merge(saddat, vegtype.key, by="plot") %>%
+  filter(is_veg > 0, category != "nonveg") %>%
+  filter(class_3 != "SF", class_3 != "ST") %>%
+  group_by(year, plot,  orig_cluster, class_3, category) %>%
+  summarize(abund=sum(abund)) %>%
+  tbl_df() 
+  
+ggplot(datcat, aes(x=year, y=abund, color=category,  group=interaction(plot, category))) + geom_line() + facet_wrap(~class_3)
+
+catsynch <- synchrony(datcat, time.var="year", species.var = "category", abundance.var="abund", replicate.var="plot")
+catsynch2 <- merge(catsynch, vegtype.key, by="plot") %>%
+  mutate(type="category")
+
+#ggplot(catsynch2, aes(x=synchrony)) + geom_bar() + facet_wrap(~orig_cluster)
+ggplot(catsynch2, aes(x=synchrony)) + geom_bar() + facet_wrap(~class_3)
+
+
+#synchrony at the Weber family level
+#lots of Weber_family =="N/A", check this
+datweb<- merge(saddat, vegtype.key, by="plot") %>%
+  filter(is_veg > 0, category != "nonveg", !is.na(Weber_family)) %>%
+  filter(class_3 != "SF", class_3 != "ST") %>%
+  group_by(year, plot,  orig_cluster, class_3, Weber_family) %>%
+  summarize(abund=sum(abund)) %>%
+  tbl_df() 
+
+
+ggplot(datweb, aes(x=year, y=abund, color=Weber_family,  group=interaction(plot, Weber_family))) + geom_line() + facet_wrap(~class_3) + theme_bw()
+
+websynch <- synchrony(datweb, time.var="year", species.var = "Weber_family", abundance.var="abund", replicate.var="plot")
+websynch2 <- merge(websynch, vegtype.key, by="plot") %>%
+  mutate(type="family")
+
+#ggplot(websynch2, aes(x=synchrony)) + geom_bar() + facet_wrap(~orig_cluster)
+ggplot(websynch2, aes(x=synchrony)) + geom_bar() + facet_wrap(~class_3)
+
+aggsynch <- rbind(sppsynch2, catsynch2, websynch2) %>%
+  filter(class_3 != "SF", class_3 !="ST", class_3!="rock")
+ggplot(aggsynch, aes(x=synchrony)) + geom_bar() + facet_grid(type~class_3) + theme_bw()
+
+
+
+datspp2 <- datspp %>%
+  mutate(type="species") %>%
+  select(-Weber_family, - category)
+names(datspp2)[3]="mylevel"
+
+datweb2 <- datweb %>%
+  mutate(type="family") 
+names(datweb2)[5]="mylevel"
+
+datcat2 <- datcat %>%
+  mutate(type="category") 
+names(datcat2)[5]="mylevel"
+
+datgraph <- rbind(datspp2, datweb2, datcat2)  %>%
+  filter(class_3 != "SF", class_3 !="ST", class_3!="rock")
+
+
+ggplot(datgraph, aes(x=year, y=abund, color=mylevel, group=interaction(mylevel, plot))) + 
+  geom_line() + facet_grid(type~class_3) + theme_bw() + theme(legend.position="none")
+ggplot(aggsynch, aes(x=synchrony)) + geom_bar() + facet_grid(type~class_3) + theme_bw()
+
 
