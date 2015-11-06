985ce02058c6d9ed0b158542d6e3a252e85f38a5
Lauren Hallett <lauren.hallett@berkeley.edu>
2015-11-05 19:28:56 -0700

    add correlograms to visualize synchrony across plots

--- synchrony_analysis.R
+++ synchrony_analysis.R
@@ -1,41 +1,59 @@
-
-
-#synchrony at the species level
-datspp<- merge(saddat, vegtype.key, by="plot") %>%
-  filter(is_veg > 0, category != "nonveg") %>%
-  group_by(year, plot, USDA_code, orig_cluster, class_3, category, Weber_family) %>%
-  summarize(abund=sum(abund)) %>%
-  tbl_df() 
-
-#synchrony at the category level
-datcat<- merge(saddat, vegtype.key, by="plot") %>%
-  filter(is_veg > 0, category != "nonveg") %>%
-  filter(class_3 != "SF", class_3 != "ST") %>%
-  group_by(year, plot,  orig_cluster, class_3, category) %>%
-  summarize(abund=sum(abund)) %>%
-  tbl_df() 
-
 library(corrgram)
-corrgram(mtcars, order=NULL, lower.panel=panel.shade,
-         upper.panel=NULL, text.panel=panel.txt,
-         main="Car Milage Data (unsorted)")
-data(mtcars)
-head(mtcars)
 
-corsppdat <- datspp %>%
-  group_by(plot, USDA_code, class_3) %>%
+#Correlograms by functional group to visualize patterns of synchrony across NWT
+#Highlights whether different habitat types have synchronous or asynchronous responses within functional groups
+
+##Use saddat from gridComposition_datacleaning
+
+#Summarize abundance at the species and functional group level
+aggdat<- merge(saddat, vegtype.key, by="plot") %>%
+  filter(is_veg > 0, category != "nonveg") %>%
+  group_by(year, plot, USDA_code, orig_cluster, class_3, category, func) %>%
+  summarize(abund=sum(abund)) %>%
+  tbl_df() 
+
+#Summarize abundance at the functional group level
+corfuncdat <- aggdat %>%
+  group_by(year, plot, func, class_3) %>%
   summarize(abund=mean(abund)) %>%
   tbl_df() %>%
-  spread(USDA_code, abund, fill=0) %>%
-  tbl_df()
-corrgram(corsppdat[,3:113])
+  filter(class_3 != "SF", class_3 !="ST", class_3!="rock") %>%
+  mutate(plot=paste(class_3, plot, sep="_")) 
 
+#Create a pdf of correlograms by functional group
+pdf("Correlograms_byFunctionalGroup.pdf")
+funcgroups<-unique(corfuncdat$func)[-7]
 
-corgeumdat <- datspp %>%
+for (i in 1:length(funcgroups)) {
+  subfunc = funcgroups[i]
+  subdat<-corfuncdat[which(corfuncdat$func==subfunc),] %>%
+    select(-class_3) %>%
+    spread(plot, abund, fill=0) %>%
+    tbl_df()
+
+  corrgram(subdat[,3:ncol(subdat)], order=NULL, upper.panel=panel.shade,
+         lower.panel=NULL,
+         main=subfunc)
+}
+
+dev.off()
+ 
+
+#synchrony at the species level
+corsppdat <- aggdat %>%
   group_by(year, plot, USDA_code, class_3) %>%
   summarize(abund=mean(abund)) %>%
   tbl_df() %>%
+  filter(class_3 != "SF", class_3 !="ST", class_3!="rock") %>%
+  mutate(plot=paste(class_3, plot, sep="_"))
+
+#look at some example species, GEROT and DECE, etc...
+subsppdat<-corsppdat %>%
   filter(USDA_code=="DECE") %>%
+  select(-class_3) %>%
   spread(plot, abund, fill=0) %>%
   tbl_df()
-corrgram(corgeumdat[,4:47])
+
+
+corrgram(subsppdat[,3:ncol(subsppdat)], order=NULL, upper.panel=panel.shade,
+         lower.panel=NULL)
