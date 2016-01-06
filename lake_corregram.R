1e2317924a2598f41bd25bf358898a8de1997dbe
Lauren Hallett <lauren.hallett@berkeley.edu>
2016-01-06 14:19:00 -0700

    updates lake corregrams with Eric's cleaned data

--- lake_corregram.R
+++ lake_corregram.R
@@ -1,7 +1,3 @@
-##Hi Eric - I'm taking a stab at the lake data to create corrgrams of synchrony and measure the portfolio effect
-##The data are super messy - I've done a little tidying but I don't know enough about the taxa to be thorough
-##See notes below - want to add in the cleaning? Then re-run the metrics.
-##Thanks! Lauren
 
 library(tidyr)
 library(dplyr)
@@ -10,6 +6,77 @@ library(grid)
 library(gridExtra)
 library(corrgram)
 library(corrplot)
+
+##LAKE CORREGRAMS WITH ERIC'S CLEANED DATA
+##USES THE TIMEPOINT WITH MAX BIOVOLUME, AND AT A DEPTH OF 9 M
+datpath = "~/Dropbox/NWT_data/" # this likely will be different for different folks
+
+lakedat <- read.csv(file.path(datpath, 'DATA_GL4_phytos_2008-2014_9m_long.csv')) %>%
+  tbl_df() %>%
+  mutate(abund=mean_biovolume_um3_ml) 
+  
+
+
+lakedat2 <-lakedat %>%
+  select(lowest.taxon, mean_biovolume_um3_ml, year) %>%
+  spread(lowest.taxon, mean_biovolume_um3_ml, fill=0)
+#Create a correlation matrix from the data
+cormat<-cor(lakedat2[,2:ncol(lakedat2)])
+
+##VISUALIZE THE CORREGRAM
+corrplot(cormat,method="circle",type="upper", tl.col="black", tl.cex = .8, diag=F)
+
+
+
+
+phylumlevel<-lakedat %>%
+  group_by(year,  lowest.taxon) %>%
+  mutate(abund=sum(abund)) %>%
+  tbl_df() %>%
+  group_by(lowest.taxon) %>%
+  summarize(phylumtempmean=mean(abund), phylumtempvar=var(abund))
+
+
+
+alllevel<-lakedat %>%
+  group_by(year) %>%
+  mutate(abund=sum(abund)) %>%
+  tbl_df() %>%
+  summarize(alltempmean=mean(abund), alltempvar=var(abund))
+
+
+
+########## Portfolio effects
+ObsMean<-log(alllevel$alltempmean); ObsMean
+ObsVar<-log(alllevel$alltempvar); ObsVar
+
+
+### Within each Phylum
+phylumlevel$logMean<-log(phylumlevel$phylumtempmean)
+phylumlevel$logVar<-log(phylumlevel$phylumtempvar)
+phylumlevel$ObsMean<-ObsMean
+phylumlevel$ObsVar <-ObsVar
+m5<-lm(logVar~logMean,data=phylumlevel); summary(m5)
+ggplot(phylumlevel, aes(x=logMean, y=logVar)) + geom_point(pch=1) + xlim(0, ObsMean+1)+ ylim(0, ObsVar+1) +
+  geom_smooth(method="lm", se=F, fullrange = T) +
+  geom_point(aes(x=ObsMean, y=ObsVar)) + theme_classic()
+
+# plot(phylumlevel$logMean,phylumlevel$logVar,xlim=c(0,ObsMean+1),ylim=c(0,ObsVar+1)); abline(m5)
+# points(ObsMean,ObsVar,pch=16,col="black")
+PredVar<-predict(m5,data.frame(logMean=ObsMean))
+BufPHY<-PredVar/ObsVar; BufPHY
+expBufPHY<-exp(PredVar)/exp(ObsVar); expBufPHY
+##Observed variance is 22% of expected variance
+exp(ObsVar)/exp(predict(m5,data.frame(logMean=ObsMean)))
+
+# pdf("lakecor_withcleandat.pdf")
+# dev.off()
+
+
+##Hi Eric - I'm taking a stab at the lake data to create corrgrams of synchrony and measure the portfolio effect
+##The data are super messy - I've done a little tidying but I don't know enough about the taxa to be thorough
+##See notes below - want to add in the cleaning? Then re-run the metrics.
+##Thanks! Lauren
 
 datpath = "~/Dropbox/NWT_data/" # this likely will be different for different folks
 head(pcouts2)
